5dfd80ccce2410b2197639634d8618cd30d9bb49
Timster1204 <vitoskolan@duck.com>
2025-05-10 13:36:32 +0200

    blotuf.adam

---
+++ dge.py
@@ -0,0 +1,28 @@
+import turtle
+
+# Set up the screen
+turtle.bgcolor("black")  # Set the background color to black
+
+tina = turtle
+tina.shape("turtle")
+tina.speed(1)
+tina.pencolor("white")  # Set the pen color to white
+
+tina.left(90)
+tina.forward(170)
+tina.right(135)
+tina.forward(70)
+tina.right(100)
+tina.forward(100)
+tina.right(180)
+tina.forward(40)
+tina.right(80)
+tina.forward(60)
+tina.right(90)
+tina.forward(60)
+tina.right(135)
+tina.forward(80)
+tina.left(35)
+tina.forward(40)
+
+turtle.done()  # Keep the window open
