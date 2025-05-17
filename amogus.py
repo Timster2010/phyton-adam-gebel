729caf99d568dcd09d4c6fb0d7515b67c30baf6a
Timster1204 <vitoskolan@duck.com>
2025-05-17 13:34:20 +0200

    17.05.2025

---
+++ amogus.py
@@ -0,0 +1,36 @@
+import turtle
+amo = turtle.Turtle()
+amo.speed(10)
+
+amo.pencolor("red")
+
+amo.right(90)
+amo.circle(50, 180)
+amo.right(90)
+amo.forward(25)
+amo.right(90)
+amo.circle(50, 180)
+amo.forward(200)
+amo.circle(50, 90)
+amo.forward(130)
+amo.circle(50, 90)
+
+amo.speed(5)
+
+amo.pencolor("blue")
+
+amo.right(90)
+amo.circle(50, 180)
+amo.forward(30)
+amo.circle(50, 180)
+amo.forward(30)
+amo.circle(50, 180)
+amo.right(90)
+
+amo.speed(5)
+
+amo.pencolor("red")
+
+amo.forward(100)
+
+
